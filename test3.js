b475718fd1f4b51e14abe84c3e3005b24e6ae452
Kevin Qiu <gangqiu0@gmail.com>
2014-07-17 00:41:33 -0400

    completes basic vertical bar chart

--- test3.js
+++ test3.js
@@ -1,40 +1,68 @@
 (function(){
-	var chartHeight = 400, chartWidth = 400, barSpace = 5;
-	var chart = d3.select('#svg-chart');
+	var height = 500, width = 800, barSpace = 0,
+		margin = {top: 20, right: 30, bottom: 30, left: 40}
+		chartWidth = width  - margin.left - margin.right,
+		chartHeight= height - margin.top  - margin.bottom
+
+	var chart = d3.select('#svg-chart')
+		.attr({'width':  width, 'height': height })
+		.append('g')
+		.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+
 	var type = function(data) {
 		data['Frequency'] = parseFloat(data['Frequency']);
 		return data;
 	}
 
 	d3.tsv('./alphabet.tsv', type, function(data) {
-		var scale = d3.scale.linear()
+		var barWidth = chartWidth / data.length;
+
+		var y = d3.scale.linear()
 			.domain([0, d3.max(data, function(d) {return d['Frequency']})])
 			.range([chartHeight, 0]);
 
-		var barWidth = chartWidth / data.length;
-		
-		chart = chart.selectAll('g')
+		var x = d3.scale.ordinal()
+			.domain(data.map(function(d) {return d['Letter']}))
+			.rangeRoundBands([0, chartWidth], 0.1);
+
+		var xAxis = d3.svg.axis()
+			.scale(x)
+			.orient('bottom');
+
+		var yAxis = d3.svg.axis()
+			.scale(y)
+			.orient('left')
+
+		bars = chart.selectAll('.bar')
 			.data(data)
 			.enter()
-			.append('g')
-			.attr('transform', function(d,i) {return 'translate(' + i*barSpace + ',30)'})
+			// .append('g')
+			// .attr('transform', function(d,i) {return 'translate(' + i*barSpace + ',30)'})
 
-		chart.append('rect')
+		bars.append('rect')
+			.attr('class', 'bar')
 			.attr({
-				'width':  barWidth,
-				'height': function(d,i) { return chartHeight - scale(d['Frequency']); },
-				'x':      function(d,i) { return i*barWidth; },
-				'y':      function(d,i) { return scale(d['Frequency']); }
+				'width':  x.rangeBand(),
+				'height': function(d,i) { return chartHeight - y(d['Frequency']); },
+				'x':      function(d,i) { return x(d['Letter']); },
+				'y':      function(d,i) { return y(d['Frequency']); }
 			})
 
-		chart.append('text')
-			.attr({
-				'x' : function(d,i) { return barWidth * (i + 0.5)-3; },
-				'y' : chartHeight,
-				'dy': '1em'
-			})
-			.text(function(d) {return d['Letter']});
+		chart.append('g')
+				.attr({
+					'class': 'x axis',
+					'transform': 'translate(0,' + (chartHeight + 5) + ')',
+				})
+				.call(xAxis)
 
-
+		chart.append('g')
+				.attr('class', 'y axis')
+				.call(yAxis)
+			.append('text')
+				.attr('transform', 'rotate(-90)')
+				.attr('y', 6)
+				.attr('dy', '.711em')
+				.style('text-anchor', 'end')
+				.text('% Frequency')
 	});
 })()
